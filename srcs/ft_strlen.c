233e7d80d78fcd87e6181303bd898ac126b6b6ba
ffornesp <ffornesp@gmail.com>
2025-07-02 18:41:22 +0200

    Added strlen

---
+++ srcs/ft_strlen.c
@@ -0,0 +1,19 @@
+/*
+	#include <string.h>
+	size_t strlen(const char *s);
+
+	The strlen() function calculates the length of the string s, excluding the terminating 
+	null byte (aq\0aq).
+
+	The strlen() function returns the number of bytes in the string s.
+*/
+
+#include "libft.h"
+
+size_t ft_strlen(const char *s) {
+	size_t	len = 0;
+
+	while (s[len])
+		len++;
+	return len;
+}
