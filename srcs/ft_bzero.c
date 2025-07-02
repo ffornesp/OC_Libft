cc67a650799a58833eb60d1e020dbf6ca4bd3020
ffornesp <ffornesp@gmail.com>
2025-07-02 18:41:30 +0200

    Added bzero

---
+++ srcs/ft_bzero.c
@@ -0,0 +1,18 @@
+/*
+	#include <strings.h>
+	void bzero(void *s, size_t n);
+
+	The bzero() function sets the first n bytes of the area starting at s to zero 
+		(bytes containing aq\0aq).
+
+	The bzero() function doesn't return anything.
+*/
+
+#include "libft.h"
+
+void	ft_bzero(void *s, size_t n) {
+	unsigned char	*p = (unsigned char *)s;
+
+	while (n--)
+		*p++ = 0;
+}
