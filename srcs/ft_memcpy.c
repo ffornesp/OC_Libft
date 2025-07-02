40951f6af03f3fb8cbb8bcc892350b26f50830d8
ffornesp <ffornesp@gmail.com>
2025-07-02 18:40:04 +0200

    Added basic memory functions

---
+++ srcs/ft_memcpy.c
@@ -0,0 +1,21 @@
+/*
+	#include <string.h>
+	void *memcpy(void *dest, const void *src, size_t n);
+
+	The memcpy() function copies n bytes from memory area src to memory area dest.
+		The memory areas must not overlap. Use memmove(3) if the memory areas do 
+		overlap.
+
+	The memcpy() function returns a pointer to dest.
+*/
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n) {
+	unsigned char		*d = (unsigned char *)dst;
+	const unsigned char	*s = (const unsigned char *)src;
+
+	while (n--)
+		*d++ = *s++;
+	return (dst);
+}	
---
+++ srcs/ft_memset.c
@@ -0,0 +1,19 @@
+/*
+	#include <string.h>
+	void *memset(void *s, int c, size_t n);
+
+	The memset() function fills the first n bytes of the memory area pointed to by s 
+		with the constant byte c. 
+
+	The memset() function returns a pointer to the memory area s. 
+*/
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n) {
+	unsigned char	*p = (unsigned char *)s;
+
+	while (n--)
+		*p++ = (unsigned char)c;
+	return (s);
+}
