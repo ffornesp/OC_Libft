1d169556ee60081786eb102dd09a44df998593be
ffornesp <ffornesp@gmail.com>
2025-07-03 17:32:53 +0200

    Added memchr and memcmp functions

---
+++ srcs/ft_memchr.c
@@ -0,0 +1,27 @@
+/*
+	#include <string.h>
+	void *memchr(const void *s, int c, size_t n);
+
+	The memchr() function scans the initial n bytes of the memory area pointed to by s 
+		for the first instance of c. Both c and the bytes of the memory area pointed to 
+		by s are interpreted as unsigned char. 
+
+	RETURN VALUES:
+
+	The memchr() and memrchr() functions return a pointer to the matching byte or NULL 
+		if the character does not occur in the given memory area. 
+*/
+
+#include "libft.h"
+
+void *memchr(const void *s, int c, size_t n) {
+	const unsigned char	*us = (const unsigned char *)s;
+	const unsigned char	uc = (const unsigned char)c;
+
+	while (n--) {
+		if (*us == uc)
+			return str;
+		us++;
+	}
+	return NULL;
+}
---
+++ srcs/ft_memcmp.c
@@ -0,0 +1,32 @@
+/*
+	#include <string.h>
+	int memcmp(const void *s1, const void *s2, size_t n);
+
+	The memcmp() function compares the first n bytes (each interpreted as unsigned char) 
+		of the memory areas s1 and s2. 
+
+	RETURN VALUES:
+
+	The memcmp() function returns an integer less than, equal to, or greater than zero if 
+		the first n bytes of s1 is found, respectively, to be less than, to match, or be 
+		greater than the first n bytes of s2.
+
+	For a nonzero return value, the sign is determined by the sign of the difference 
+		between the first pair of bytes (interpreted as unsigned char) that differ in s1 
+		and s2. 
+*/
+
+#include "libft.h"
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+	const unsigned char *us1 = (const unsigned char *)s1;
+	const unsigned char *us2 = (const unsigned char *)s2;
+	
+	while (n--) {
+		if (*us1 != *us2)
+			return (int)(*us1 - *us2);
+		us1++;
+		us2++;
+	}
+	return 0;
+}
