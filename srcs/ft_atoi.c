7d06dee15bcc1f717e4559d3e24c7273c013f5ce
ffornesp <ffornesp@gmail.com>
2025-07-03 17:37:39 +0200

    Added base for atoi

---
+++ srcs/ft_atoi.c
@@ -0,0 +1,18 @@
+/*
+	#include <stdlib.h>
+	int atoi(const char *nptr);
+
+	The atoi() function converts the initial portion of the string pointed to by 
+		nptr to int. The behavior is the same as strtol(nptr, NULL, 10); except 
+		that atoi() does not detect errors. 
+
+	RETURN VALUES:
+
+	The converted value.
+*/
+
+#include "libft.h"
+
+int	atoi(const char *nptr) {
+
+}
