4799b2263f0da9b28c4cb39f09a8853f8fa33abd
ffornesp <ffornesp@gmail.com>
2025-07-03 17:38:09 +0200

    Added atoi to header file

--- srcs/libft.h
+++ srcs/libft.h
@@ -50,5 +50,6 @@ int		strncmp(const char *s1, const char *s2, size_t n);
 void	*memchr(const void *s, int c, size_t n);
 int		memcmp(const void *s1, const void *s2, size_t n);
 char	*strnstr(const char *big, const char *little, size_t len);
+int		atoi(const char *nptr);
 
 #endif
