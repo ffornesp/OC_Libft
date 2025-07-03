04564f1d4e83f6994a2708deaa97bf5a961885c5
ffornesp <ffornesp@gmail.com>
2025-07-03 17:33:10 +0200

    Updated header

--- srcs/libft.h
+++ srcs/libft.h
@@ -47,5 +47,8 @@ int		tolower(int c);
 char	*strchr(const char *s, int c);
 char	*strrchr(const char *s, int c);
 int		strncmp(const char *s1, const char *s2, size_t n);
+void	*memchr(const void *s, int c, size_t n);
+int		memcmp(const void *s1, const void *s2, size_t n);
+char	*strnstr(const char *big, const char *little, size_t len);
 
 #endif
