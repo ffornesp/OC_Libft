cca64f7c1c93914956d298ee211596584c9ea3e6
ffornesp <ffornesp@gmail.com>
2025-07-04 16:03:16 +0200

    Added base for strjoin.c

---
+++ srcs/ft_strjoin.c
@@ -0,0 +1,17 @@
+/*
+	#include "libft.h"
+	char *ft_strjoin(char const *s1, char const *s2);
+
+	The ft_strjoin() function allocates memory (using malloc(3)) and returns a
+		new string, which is the result of concatenating 's1' and 's2'.
+
+	RETURN VALUE:
+
+	The new string or NULL if the allocation fails.
+*/
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2) {
+	
+}
