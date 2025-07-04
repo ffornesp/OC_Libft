fcbd2d991dcd36e5d5ce4f72abd621b881c82f11
ffornesp <ffornesp@gmail.com>
2025-07-04 16:24:18 +0200

    Added base for strtrim

---
+++ srcs/ft_strtrim.c
@@ -0,0 +1,18 @@
+/*
+	#include "libft.h"
+	char *ft_strtrim(char const *s1, char const *set);
+
+	The ft_strtrim() function allocates memory (using malloc(3)) and returns a
+		copy of 's1' with characters from 'set' removed from the beggining and
+		the end.
+
+	RETURN VALUE:
+
+	The trimmed string or NULL if the allocation fails.
+*/
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set) {
+
+}
