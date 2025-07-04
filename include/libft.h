5da60cab709639070f404c78ba5b964c65785ceb
ffornesp <ffornesp@gmail.com>
2025-07-04 16:03:30 +0200

    Added strjoin to header file

--- include/libft.h
+++ include/libft.h
@@ -60,5 +60,6 @@ char	*ft_strdup(const char *s);
 	/*		OTHER functions		*/
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
