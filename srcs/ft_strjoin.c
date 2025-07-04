59a7a3856df3806c7c04cb8bfe95bb295dfb51e7
ffornesp <ffornesp@gmail.com>
2025-07-04 16:21:44 +0200

    Added strjoin

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -11,7 +11,28 @@
 */
 
 #include "libft.h"
+#include <limits.h>
 
 char	*ft_strjoin(char const *s1, char const *s2) {
+	if (!s1 && !s2)
+		return ft_strdup("");
+	else if (!s1)
+		return ft_strdup(s2);
+	else if (!s2)
+		return ft_strdup(s1);
+
+	unsigned long long	total_len = (unsigned long long)ft_strlen(s1) + (unsigned long long)ft_strlen(s2);
+	if (total_len > SIZE_MAX)
+		return NULL;
+
+	char	*str = ft_calloc(total_len + 1, sizeof(char));
+	if (!str)
+		return NULL;
 	
+	size_t	i = 0;
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	return str;
 }
