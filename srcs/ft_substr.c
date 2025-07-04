eb37d1ef4ba60d8a8135ee10d15811cb6213ec20
ffornesp <ffornesp@gmail.com>
2025-07-04 15:32:24 +0200

    Added base for ft_substr

---
+++ srcs/ft_substr.c
@@ -0,0 +1,19 @@
+/*
+	#include "libft.h"
+	char *ft_substr(char const *s, unsigned int start, size_t len);
+
+	The ft_substr() function allocates memory (using malloc(3)) and returns a
+		substring from the string 's'. 
+	
+	The substring starts at index 'start' and has a maximum length of 'len'.
+
+	RETURN VALUE:
+
+	The substring or NULL if the allocation fails.
+*/
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len) {
+	
+}
