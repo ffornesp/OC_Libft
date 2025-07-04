869a9ce0bcc33823f9439d08b06c718818b449a0
ffornesp <ffornesp@gmail.com>
2025-07-04 13:36:57 +0200

    Fixed atoi

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -14,49 +14,30 @@
 #include "libft.h"
 #include <stdbool.h>
 
-static int	ft_pow(int n, size_t times) {
-	int	res = n;
-
-	if (times < 1)
-		return 1;
-
-	while (times-- > 1)
-		res *= n;
-
-	return res;
-}
-
-static size_t	count_digits(const char *str) {
-	size_t	len = 0;
-
-	while (str[len] && ft_isdigit(str[len]))
-		len++;
-	return len;
+static bool	ft_isspace(const char c) {
+	if (c == ' ' || (c >= 9 && c <= 13))
+		return true;
+	return false;
 }
 
 int	ft_atoi(const char *nptr) {
 	bool	negative = false;
-
-	if (!ft_isdigit(*nptr)) {
-		if (*nptr == '-')
-			negative = true;
-		else if (*nptr != '+')
-			return 0;
-		nptr++;
-	}
-
-	size_t	len = count_digits(nptr);
 	int		res = 0;
 	size_t	i = 0;
 
-	while (len) {
-		if (!ft_isdigit(nptr[i]))
-			break ;
-		res += ((int)nptr[i++] - '0') * ft_pow(10, --len);
-		if (i >= 20)
-			return -1;
-	}
+	while (*nptr && ft_isspace(*nptr))
+		nptr++;
 
+	if (*nptr == '-' || *nptr == '+')
+		negative = (*nptr++ == '-');
+
+	while (*nptr) {
+		if (!ft_isdigit(*nptr))
+			break ;
+		res = res * 10 + *nptr++ - '0';
+		if (i++ >= 20)
+			return -1 + negative;
+	}
 	if (negative == true)
 		res *= -1;
 
