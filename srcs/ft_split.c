16f06d08fc8d1bb96069ebb0295b0ee16184f537
ffornesp <ffornesp@gmail.com>
2025-07-04 17:55:32 +0200

    Added base for split

---
+++ srcs/ft_split.c
@@ -0,0 +1,19 @@
+/*
+	#include "libft.h"
+	char	**ft_split(char const *s, char c);
+
+	The ft_split() function allocates memory (using malloc(3)) and returns an
+		array of strings obtained by splitting 's' using the character 'c' as
+		a delimiter. The array must end with a NULL pointer.
+
+	RETURN VALUE:
+
+	The array of new strings resulting from the split. NULL if the allocation
+		fails.
+*/
+
+#include "libft.h"
+
+char	**ft_split(char const *s, char c) {
+	
+}
