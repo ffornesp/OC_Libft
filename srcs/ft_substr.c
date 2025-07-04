9f538e52d749eca24719ca4c5d078c0d2ada53e2
ffornesp <ffornesp@gmail.com>
2025-07-04 15:55:33 +0200

    Implemented ft_substr.c

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -15,5 +15,21 @@
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len) {
-	
+	if (!s)
+		return NULL;
+
+	size_t	slen = ft_strlen(s);
+	if (start > slen)
+		return ft_strdup("");
+
+	size_t	size;
+	if (slen - start >= len)
+		size = len;
+	else
+		size = slen - start;
+
+	char	*str = ft_calloc(size + 1);
+	if (!str)
+		return NULL;
+	return ft_memcpy(str, s + start, size);
 }
