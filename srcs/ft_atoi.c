857715a2aed01df155edae7c8ac0cbbe8d2fe6cf
ffornesp <ffornesp@gmail.com>
2025-07-04 13:08:18 +0200

    Added atoi

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -12,7 +12,53 @@
 */
 
 #include "libft.h"
+#include <stdbool.h>
 
-int	atoi(const char *nptr) {
+static int	ft_pow(int n, size_t times) {
+	int	res = n;
 
+	if (times < 1)
+		return 1;
+
+	while (times-- > 1)
+		res *= n;
+
+	return res;
+}
+
+static size_t	count_digits(const char *str) {
+	size_t	len = 0;
+
+	while (str[len] && ft_isdigit(str[len]))
+		len++;
+	return len;
+}
+
+int	ft_atoi(const char *nptr) {
+	bool	negative = false;
+
+	if (!ft_isdigit(*nptr)) {
+		if (*nptr == '-')
+			negative = true;
+		else if (*nptr != '+')
+			return 0;
+		nptr++;
+	}
+
+	size_t	len = count_digits(nptr);
+	int		res = 0;
+	size_t	i = 0;
+
+	while (len) {
+		if (!ft_isdigit(nptr[i]))
+			break ;
+		res += ((int)nptr[i++] - '0') * ft_pow(10, --len);
+		if (i >= 20)
+			return -1;
+	}
+
+	if (negative == true)
+		res *= -1;
+
+	return res;
 }
